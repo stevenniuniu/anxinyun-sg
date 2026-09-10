e4a83efab5c1c5ac524348622ea79b3bf97328b6
MULU <35390640+stevenniuniu@users.noreply.github.com>
2026-09-10 04:48:44 +0000

    feat: build bilingual Anxinyun movers landing page

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,6 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = "Astro Blog";
-export const SITE_DESCRIPTION = "Welcome to my website!";
+export const SITE_TITLE = "安心运 | Anxinyun Singapore Movers";
+export const SITE_DESCRIPTION =
+	"安心运 Anxinyun 提供新加坡本地搬家、办公室搬迁与家具运输服务。";
